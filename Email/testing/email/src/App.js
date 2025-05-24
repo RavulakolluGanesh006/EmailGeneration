546820a69e487282fa1cb2a5fc3ab57800c980df
nikhil <ravulakolluganesh06@gmail.com>
2025-05-24 18:00:48 +0530

    intial commit

---
+++ Email/testing/email/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Generationform from './Component/Generationform';
+
+function App() {
+  return (
+    <div className="App">
+      <Generationform/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Email/testing/email/src/Component/Generationform.jsx
@@ -0,0 +1,5 @@
+export default function Generationform(){
+    return(
+<h1>email generator</h1>
+    )
+}
